8b572afe25d8a8b4565d0d470db1dca921d2ec71
unknown <ismayito1@gmail.com>
2023-03-07 16:23:39 +0300

    Add popup window

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
-const menuBtn = document.getElementById('menu-close-btn');
-const menuSection = document.querySelector('section.mobile-menu');
-const closeButton = document.getElementById('close-btn');
-const menuItems = document.getElementsByClassName('menu-item');
+const menuBtn = document.getElementById("menu-close-btn");
+const menuSection = document.querySelector("section.mobile-menu");
+const closeButton = document.getElementById("close-btn");
+const menuItems = document.getElementsByClassName("menu-item");
 
 for (let i = 0; i < menuItems.length; i += 1) {
-  menuItems[i].addEventListener('click', () => {
-    menuSection.classList.remove('visible');
+  menuItems[i].addEventListener("click", () => {
+    menuSection.classList.remove("visible");
   });
 }
 
-menuBtn.addEventListener('click', () => {
-  menuSection.classList.add('visible');
+menuBtn.addEventListener("click", () => {
+  menuSection.classList.add("visible");
 });
 
-closeButton.addEventListener('click', () => {
-  menuSection.classList.remove('visible');
+closeButton.addEventListener("click", () => {
+  menuSection.classList.remove("visible");
 });
+
+// project-details-mobile
+
+const projectDetails = [
+  {
+    title: "Tonic",
+    technologies: ["CANOPY", "Back end", 2015],
+    img: "assets/images/Counter.svg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ["html", "css", "javascript"],
+    liveLink: "https://ismayito.github.io/Portfolio-/",
+    sourceCode: "https://github.com/ismayito/Portfolio-",
+  },
+];
