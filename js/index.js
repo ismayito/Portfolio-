68b92dceb6c9cef7b7f56f0538040707a0931c9c
unknown <ismayito1@gmail.com>
2023-03-08 12:58:42 +0300

    Add popup window to project

--- js/index.js
+++ js/index.js
@@ -1,94 +1,96 @@
-const menuBtn = document.getElementById("menu-close-btn");
-const menuSection = document.querySelector("section.mobile-menu");
-const closeButton = document.getElementById("close-btn");
-const menuItems = document.getElementsByClassName("menu-item");
-const OpenProjectBtn = document.getElementsByClassName("see-project");
-const PopUp = document.querySelector("section.modal-mobile");
-const ClosePopUp = document.querySelector("img.close-window");
-
-// for (let i = 0; i < OpenProjectBtn.length; i += 1) {
-//   OpenProjectBtn[i].addEventListener("click", () => {
-//     PopUp.classList.add("visible");
-//   });
-// }
-
-// ClosePopUp.addEventListener("click", function () {
-//   PopUp.classList.remove("visible");
-// });
+const menuBtn = document.getElementById('menu-close-btn');
+const menuSection = document.querySelector('section.mobile-menu');
+const closeButton = document.getElementById('close-btn');
+const menuItems = document.getElementsByClassName('menu-item');
 
 for (let i = 0; i < menuItems.length; i += 1) {
-  menuItems[i].addEventListener("click", () => {
-    menuSection.classList.remove("visible");
+  menuItems[i].addEventListener('click', () => {
+    menuSection.classList.remove('visible');
   });
 }
 
-menuBtn.addEventListener("click", () => {
-  menuSection.classList.add("visible");
+menuBtn.addEventListener('click', () => {
+  menuSection.classList.add('visible');
 });
 
-closeButton.addEventListener("click", () => {
-  menuSection.classList.remove("visible");
+closeButton.addEventListener('click', () => {
+  menuSection.classList.remove('visible');
 });
 
-// project-details-mobile
+// project-details-window-popup
 
 const data = [
   {
     id: 1,
-    title: "Tonic",
-    company: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
-    img: "assets/images/Counter.svg",
+    title: 'Tonic',
+    company: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
+    img1: 'assets/images/project-1.svg',
+    img: 'assets/images/Snapshoot Portfolio.svg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    languages: ["html", "css", "javascript"],
-    liveLink: "https://ismayito.github.io/Portfolio-/",
-    sourceCode: "https://github.com/ismayito/Portfolio-",
+    languages: ['html', 'css', 'javascript'],
+    liveLink: 'https://ismayito.github.io/Portfolio-/',
+    sourceCode: 'https://github.com/ismayito/Portfolio-',
   },
   {
     id: 2,
-    title: "Multi-Post Stories",
-    company: "FACEBOOK",
-    role: "Full Stack Dev",
-    year: "2015",
-    img: "assets/images/project-4.svg",
+    title: 'Multi-Post Stories',
+    company: 'FACEBOOK',
+    role: 'Full Stack Dev',
+    year: '2015',
+    img1: 'assets/images/project-2.svg',
+    img: 'assets/images/project-4.svg',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    languages: ["html", "Ruby on rails", "css", "javascript"],
-    liveLink: "https://ismayito.github.io/Portfolio-/",
-    sourceCode: "https://github.com/ismayito/Portfolio-",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    languages: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: 'https://ismayito.github.io/Portfolio-/',
+    sourceCode: 'https://github.com/ismayito/Portfolio-',
   },
   {
     id: 3,
-    title: "Facebook 360",
-    company: "FACEBOOK",
-    role: "Full Stack Dev",
-    year: "2015",
-    img: "assets/images/project-1.svg",
+    title: 'Facebook 360',
+    company: 'FACEBOOK',
+    role: 'Full Stack Dev',
+    year: '2015',
+    img1: 'assets/images/project-3.svg',
+    img: 'assets/images/project-1.svg',
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    languages: ["html", "Ruby on rails", "css", "javascript"],
-    liveLink: "https://ismayito.github.io/Portfolio-/",
-    sourceCode: "https://github.com/ismayito/Portfolio-",
+    languages: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: 'https://ismayito.github.io/Portfolio-/',
+    sourceCode: 'https://github.com/ismayito/Portfolio-',
   },
   {
     id: 4,
-    title: "Uber Navigation",
-    company: "Uber",
-    role: "Lead Developer",
-    year: "2018",
-    img: "assets/images/project-1.svg",
+    title: 'Uber Navigation',
+    company: 'Uber',
+    role: 'Lead Developer',
+    year: '2018',
+    img1: 'assets/images/project-4.svg',
+    img: 'assets/images/project-2.svg',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    languages: ["html", "Ruby on rails", "css", "javascript"],
-    liveLink: "https://ismayito.github.io/Portfolio-/",
-    sourceCode: "https://github.com/ismayito/Portfolio-",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    languages: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: 'https://ismayito.github.io/Portfolio-/',
+    sourceCode: 'https://github.com/ismayito/Portfolio-',
   },
 ];
 
-
-function createPopUp(id,title, company, description,role,year,img,languages,liveLink) {
+function createPopUp(
+  id,
+  title,
+  company,
+  description,
+  role,
+  year,
+  img1,
+  img,
+  languages,
+  liveLink,
+  sourceCode,
+) {
   return `
   <section class="modal-mobile" id="popup-project-${id}">
         <div class="modal-head">
@@ -101,7 +103,7 @@ function createPopUp(id,title, company, description,role,year,img,languages,live
           />
         </div>
         <ul class="categories">
-          <li class="item1">CANOPY</li>
+          <li class="item1">${company}</li>
           <li class="counter">
             <img
               class="img-counter"
@@ -118,13 +120,13 @@ function createPopUp(id,title, company, description,role,year,img,languages,live
           </li>
         </ul>
         <img
-          src="assets/images/project-1.svg"
+          src="${img1}"
           class="project-1 project"
           alt="project-one"
         />
 
         <img
-          src="assets/images/Snapshoot Portfolio.svg"
+          src="${img}"
           class="project-1 project-1-desktop"
           id="project1-desktop"
           alt="project-one"
@@ -135,47 +137,60 @@ function createPopUp(id,title, company, description,role,year,img,languages,live
           </article>
           <div class="lang-buttons">
             <ul class="languages">
-              <li>${languages[0]}</li>
-              <li>${languages[1]}</li>
-              <li>${languages[2]}</li>
+            ${languages.map((item) => `<li>${item}</li>`).join('')}
             </ul>
             <img src="assets/images/vector 4.svg" alt="line" class="line" />
             <div class="window-buttons">
               <button class="window-btn">
+              <a href='${liveLink}' class="PopUp-link">
                See live
                 <span><img src="assets/images/live.svg" alt="live" /></span>
-           
+                
               </button>
               <button class="window-btn">
+              <a href='${sourceCode}' class="PopUp-link">
                 See source
                 <span
                   ><img src="assets/images/git-modal.svg" alt="source code"
                 /></span>
+              </a>
               </button>
             </div>
           </div>
         </div>
       </section>
-  `
+  `;
 }
 
-var final = ""
-data.forEach(item =>{
-  const html = createPopUp(item.id, item.title,item.company, item.description, item.role, item.year, item.img , item.languages, item.liveLink)
+let final = '';
+data.forEach((item) => {
+  const html = createPopUp(
+    item.id,
+    item.title,
+    item.company,
+    item.description,
+    item.role,
+    item.year,
+    item.img1,
+    item.img,
+    item.languages,
+    item.liveLink,
+    item.sourceCode,
+  );
   final += html;
-})
+});
 
 document.getElementById('popup-container').innerHTML = final;
 
 // event listeners
-data.forEach(item=>{
+data.forEach((item) => {
   // open event
-  const id = item.id;
-  document.getElementById("openpop-" + id).addEventListener("click", function(){
-    document.getElementById("popup-project-" + id).classList.add("visible");
-  })
+  const { id } = item;
+  document.getElementById(`openpop-${id}`).addEventListener('click', () => {
+    document.getElementById(`popup-project-${id}`).classList.add('visible');
+  });
   // close event
-  document.getElementById("close-"+id).addEventListener('click', function(){
-    document.getElementById("popup-project-" + id).classList.remove("visible");
-  })
-})
+  document.getElementById(`close-${id}`).addEventListener('click', () => {
+    document.getElementById(`popup-project-${id}`).classList.remove('visible');
+  });
+});
