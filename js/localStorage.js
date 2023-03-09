6a7777056f790e6606295bc68a9bda949f51c5c4
unknown <ismayito1@gmail.com>
2023-03-09 15:43:42 +0300

    Store user input in local storage

---
+++ js/localStorage.js
@@ -0,0 +1,4 @@
+const userEmail = document.getElementById("email").value;
+const userName = document.getElementById("name").value;
+const userMessage = document.getElementById("message").value;
+const myform = document.getElementById("form-contact");
--- js/validate.js
+++ js/validate.js
@@ -1,12 +1,12 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const error = document.getElementById('error-message');
-const msg = 'Please your email must be in lowercase letters';
+const form = document.getElementById("form-contact");
+const email = document.getElementById("email");
+const error = document.getElementById("error-message");
+const msg = "Please your email must be in lowercase letters";
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   if (email.value !== email.value.toLowerCase()) {
     e.preventDefault();
-    error.className = 'error-message';
+    error.className = "error-message";
     error.textContent = msg;
   }
 });
