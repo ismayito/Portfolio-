819dbff51ff1f3800678932d3f2dea5e65186204
unknown <ismayito1@gmail.com>
2023-03-09 11:27:44 +0300

    Finish js client validation

--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,12 @@
-const form = document.getElementById("form");
-const email = document.getElementById("email");
-const error = document.getElementById("error-message");
-let msg = "Please your email must be in lowercase letters";
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.getElementById('error-message');
+const msg = 'Please your email must be in lowercase letters';
 
-function errorMessage(input, message, type) {
-  error.innerHTML = msg;
-  input.className = type ? "error-message" : "error";
-
-  return type;
-}
-
-function showSucess(input, message) {
-  return errorMessage(input, "", false);
-}
-
-// function showError(input, message) {
-//   return errorMessage(input, msg, true);
-// }
-
-
-
-form.addEventListener('submit', function(e){
-  if (email.value != email.value.toLowerCase()){
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
     e.preventDefault();
+    error.className = 'error-message';
     error.textContent = msg;
-    error.className = "error-message";
   }
-})
+});
