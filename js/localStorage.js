fedd87c7aa23447feb446fa3d6515bb12a284941
unknown <ismayito1@gmail.com>
2023-03-09 17:02:34 +0300

    Store user input data to local storage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,10 +1,28 @@
-const userEmail = document.getElementById("email").value;
-const userName = document.getElementById("name").value;
-const userMessage = document.getElementById("message").value;
-const myform = document.getElementById("form-contact");
+const userEmail = document.getElementById('email').value;
+const userName = document.getElementById('name').value;
+const userMessage = document.getElementById('message').value;
+const myform = document.getElementById('form-contact');
 
 const userInput = {
-    name: userName,
-    email: userEmail,
-    meassage: userMessage,
+  name: userName,
+  email: userEmail,
+  meassage: userMessage,
+};
+
+function store() {
+  localStorage.setItem('user', JSON.stringify(userInput));
+}
+
+myform.addEventListener('change', (e) => {
+  e.preventDefault(e);
+  store();
+});
+
+window.onload = function storedData() {
+  if (localStorage.length !== 0) {
+    const retievedData = JSON.parse(localStorage.getItem('user'));
+    userName.value = retievedData.name;
+    userEmail.value = retievedData.email;
+    userMessage.value = retievedData.meassage;
+  }
 };
