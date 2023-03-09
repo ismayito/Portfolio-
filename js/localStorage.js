d7c3b06ffcb3351f7e289c2df4a37c82fb02b371
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-03-09 17:19:34 +0430

    Add form object

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,3 +2,9 @@ const userEmail = document.getElementById("email").value;
 const userName = document.getElementById("name").value;
 const userMessage = document.getElementById("message").value;
 const myform = document.getElementById("form-contact");
+
+const userInput = {
+    name: userName,
+    email: userEmail,
+    meassage: userMessage,
+};
